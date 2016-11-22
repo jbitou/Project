660e8972e9cdae32dc65f9b00bdce9e998304937
GeoAndr <tzwrtzina2711994@gmail.com>
2016-11-22 17:40:07 +0200

    wtfgit add assignment.c!

--- part2/assignment.c
+++ part2/assignment.c
@@ -63,14 +63,15 @@ pcluster matrix_simplest_assignment(pcluster clusters, int **distances, hash_tab
 }
 
 pcluster matrix_reverse_approach(pcluster clusters, int **distances, hash_table *htable, ghashp *g, centroid *centroids, int k, int num_of_hash, int N, int L) {
-	int i, j, z, radii, pos, done, all, previous, id, distance, distance1, distance2, mindistance, mincentroid, *qdata;
+	int i, j, z, radii, pos, done, all, previous, id, distance, distance1, distance2, mincentroid, *qdata;
+	double mindistance;
 	chainp **barriers, temp, temp1, temp2;
 	/**L tables of pointers**/
 	barriers = malloc(L*sizeof(chainp *));
-	for (i=0; i < L; i++) 
-		barriers[i] = malloc(pow(2,num_of_hash)*sizeof(chainp));
+	for (i=0; i < L; i++)	barriers[i] = malloc(pow(2,num_of_hash)*sizeof(chainp));
 	radii = matrix_compute_start_radius(distances,centroids,k);
 	done = all = 0;
+	/**Range Search**/
 	do {
 		/**For each cluster**/
 		previous = done;
@@ -112,30 +113,38 @@ pcluster matrix_reverse_approach(pcluster clusters, int **distances, hash_table 
 			if (jump != 1)	temp1 = temp1->next;
 		}
 	}
+	printf("DONE HERE\n");
 	/**For all unassigned points, compute its distances to all centroids**/
-	/**For each bucket**/
-	/*for (i=0; i < htable->size; i++) {
-		printf("FOOOOOOOR\n");
-		temp = htable->table[i];
+	/**For each bucket**/	
+	for (i=0; i < htable->size; i++) {
+		temp = htable[0].table[i];
 		/**For each item**/
-		/*while (temp != NULL) {
+		while (temp != NULL) {
+			/**Assigned points begin in the chain when the barrier is found**/
+			printf("FOR %s\n",temp->key);
 			if ((barriers[0][i] != NULL) && (strcmp(temp->key,barriers[0][i]->key) == 0))  {
 				printf("mphka gia barrier: %s!!!!\n",barriers[0][i]->key);
 				break;
 			}
 			id = make_item(temp->key) - 1;
 			/**Check if item is barrier through hash tables**/
-			/*qdata = malloc(N*sizeof(int));
+			qdata = malloc(N*sizeof(int));
 			/**Create info line with distances**/
-			/*for (z=0; z < id; z++) qdata[z] = distances[z][id-z-1];
+			for (z=0; z < id; z++) {
+				//printf("z = %d, id = %d\n",z,id);
+				qdata[z] = distances[z][id-z-1];
+			}
+			//printf("z=%d\n",z);
 			qdata[id] = 0;
 			for (z=(id+1); z < N; z++) qdata[z] = distances[id][z-id-1];
-			for (z=0; z < N; z++)	printf("qdata[%d]=%d for %s\n",z,qdata[z],temp->key);
+			//for (z=0; z < N; z++)	printf("qdata[%d]=%d for %s\n",z,qdata[z],temp->key);
 			for (j=1; j < L; j++) {
 				//pos = hash_func_MSearch(g[j],(int *)centroids[i].info,distances,num_of_hash,N);
 			}
+			free(qdata);
+			printf("DONE WITH QDATA\n");
 			/**For each centroid**/
-			/*if (id < (int)(intptr_t)centroids[0].center)  mindistance = distances[id][(int)(intptr_t)centroids[0].center-id-1];
+			if (id < (int)(intptr_t)centroids[0].center)  mindistance = distances[id][(int)(intptr_t)centroids[0].center-id-1];
 			else if (id > (int)(intptr_t)centroids[0].center)  mindistance = distances[(int)(intptr_t)centroids[0].center][id-(int)(intptr_t)centroids[0].center-1];
 			mincentroid = 0;
 			for (j=1; j < k; j++) {
@@ -149,7 +158,7 @@ pcluster matrix_reverse_approach(pcluster clusters, int **distances, hash_table 
 			insert_chain(temp->key,NULL,&(clusters[mincentroid].items),mindistance,3,0,0);
 			temp = temp->next;
 		}
-	}*/
+	}
 	for (i=0; i < L; i++)	free(barriers[i]);
 	free(barriers);
 	return clusters;
@@ -157,7 +166,7 @@ pcluster matrix_reverse_approach(pcluster clusters, int **distances, hash_table 
 
 int matrix_compute_start_radius(int **distances, centroid *centroids, int k) {
 	int i, j, distance1, distance2, mindistance;
-	/**Suppose that at least two centroids exist! First, mindistance is distance of first two centroids 0,1**/
+	/**Suppose that at least two centroids/clusters exist! First, mindistance is distance of first two centroids [0],[1]**/
 	if (centroids[0].center < centroids[1].center)  
 		mindistance = distances[(int)(intptr_t)centroids[0].center][(int)(intptr_t)centroids[1].center-(int)(intptr_t)centroids[0].center-1];
 	else if (centroids[0].center > centroids[1].center)  
@@ -183,9 +192,11 @@ int matrix_compute_objective_function(pcluster clusters, int **distances, int k)
 	int J = 0, i, id; 
 	centroid center;
 	chainp temp;
+	/**For each cluster**/
 	for (i=0; i < k; i++) {
 		temp = clusters[i].items;
 		center = clusters[i].center;
+		/**For each item in cluster**/
 		while (temp != NULL) {
 			id = make_item(temp->key) - 1;
 			if (id < (int)(intptr_t)center.center)  J += distances[id][(int)(intptr_t)center.center-id-1];
