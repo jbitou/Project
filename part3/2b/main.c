b3ca53c27affaee83e69667a5b37342ddd3f8e7b
GeoAndr <tzwrtzina2711994@gmail.com>
2016-12-27 22:32:29 +0200

    just in the beginning

---
+++ part3/2b/main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "inputProcessing.h"
+#define EXPERK 10
+
+int main (int argc, char **argv) {
+	FILE *fp, *fe;
+	char read[12];
+	int i, j, flag, input, T, numConform, N, size, k, bestk, counter = 0;
+	double **data, v1, v2, v3, silhouette, previousS, bestS;
+	//pcluster clusters, bestclusters;
+	if (command_processing(argc) == -1)	return -1;
+	for (i=1; i < (argc - 1); i+=2) {
+		if (strcmp(argv[i],"-d") == 0)	input = i+1;
+		else if (strcmp(argv[i],"-T") == 0)	T = atoi(argv[i+1]);
+	}
+	/**Open input file**/
+	fp = fopen(argv[input],"r");
+	if (fp == NULL) {
+		perror("Error opening input_file");
+		return -1;
+	}
+	/**Open output file**/
+	fe = fopen("experim.dat","w+");
+	if (fe == NULL) {
+		perror("Error opening output_file");
+		return -1;
+	}
+	fscanf(fp,"%s%d\n",read,&numConform);
+	fscanf(fp,"%s%d\n",read,&N);
+	if (numConform > 100) k = 10;
+	else k = 2;
+	size = numConform*N;
+	printf("numconform=%d and N=%d\n",numConform,N);
+	data = malloc(size*sizeof(double *));
+	for (i=0; i < size; i++)	data[i] = malloc(3*sizeof(double));
+	i = 0;
+	while (fscanf(fp,"%lf %lf %lf[^\n]",&v1,&v2,&v3) != EOF) {
+		data[i][0] = v1;
+		data[i][1] = v2;
+		data[i][2] = v3;
+		i++;
+	}
+	for (i=0; i < size; i++)	printf("[%lf,%lf,%lf]\n",data[i][0],data[i][1],data[i][2]);
+	for (i=0; i < size; i++)	free(data[i]);
+	free(data);
+	fclose(fp);
+	fclose(fe);
+	return 0;
+}
